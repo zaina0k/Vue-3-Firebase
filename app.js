cb44a59bc072ef53f1b8a8aede3513d0079829b9
Muhammad Altaf <muhammad.altaf-2@student.manchester.ac.uk>
2023-07-17 15:39:51 +0100

    data binding with the anchor tag

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://google.com",
       showBooks: true,
       books: [
         { title: 'name of the wind', author: 'patrick rothfuss' },
